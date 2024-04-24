c2e72cd6dcd2b404e8c005f04f4f0e6ac4fb1a59
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-04-25 00:33:55 +0300

    Added login button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col p-5 gap-5">
-      <div className="flex items-center gap-7">
+      <div className="flex items-center gap-8">
         <div className="flex items-center gap-2">
           <Image
             alt="App Logo"
@@ -34,11 +34,17 @@ export default async function Home() {
         <div className="relative flex-grow">
           <IoSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-700" />
           <input
-            className="w-full border border-gray-300 rounded-md shadow pl-12 pr-3 py-3 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="w-full border border-gray-300 rounded-lg shadow-lg pl-12 pr-3 py-3 focus:outline-none focus:ring-2 focus:ring-indigo-500"
             placeholder={t("searchPlaceholder")}
             type="search"
           />
         </div>
+        <button className="px-4 py-3 rounded border border-gray-400 transition duration-150 ease-in-out hover:border-black focus:border-black">
+          {t("CreateCompany")}
+        </button>
+        <button className="px-2 py-3 transition-colors duration-150 hover:text-green-800">
+          {t("LogIn")}
+        </button>
       </div>
       {categories.map(category => (
         <div key={category.id}>{category.name}</div>
--- src/langs/index.ts
+++ src/langs/index.ts
@@ -9,6 +9,8 @@ export function initLangs() {
     resources: {
       en: {
         translation: {
+          CreateCompany: "Create a company",
+          LogIn: "Log In",
           ZeroCompany: "Zero Company",
           appDescription:
             "Create a virtual company at zero cost and grow it into your first unicorn!",
