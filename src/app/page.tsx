ac7c136fdaca98a3aabecc2094f9fa77f997e21e
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-04-26 22:02:04 +0300

    Added featured carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Bebas_Neue } from "next/font/google";
 import Image from "next/image";
 import { IoSearch } from "react-icons/io5";
 import React from "react";
+import { assertDefined } from "../utils";
 import { images } from "../images";
 import { lang } from "../langs";
 
@@ -36,9 +37,9 @@ export default async function Home(): Promise<React.ReactElement> {
   ];
 
   return (
-    <div className="flex flex-col gap-9">
+    <div className="flex flex-col">
       {/* Header */}
-      <div className="border header-border-width flex flex-col p-5 gap-5">
+      <div className="border-b-1.5 p-5 flex flex-col gap-5">
         {/* Main header */}
         <div className="flex items-center gap-8">
           {/* Logo */}
@@ -92,25 +93,25 @@ export default async function Home(): Promise<React.ReactElement> {
       </div>
       {/* Header END */}
 
-      {/* Teaser */}
-      <h2 className="text-center text-xl text-gray-500">{lang.home.teaser}</h2>
-      {/* Teaser END */}
+      {/* Contents */}
+      <div className="self-center m-9 max-w-screen-lg flex flex-col gap-9">
+        {/* Teaser */}
+        <h2 className="text-center text-xl text-gray-500">
+          {lang.home.teaser}
+        </h2>
+        {/* Teaser END */}
 
-      {/* Cards */}
-      <div className="flex justify-center">
+        {/* Cards */}
         <div className="grid grid-cols-3 gap-3">
           {cards.map(({ description, image, title }, key) => (
-            <div
-              className="max-w-xs rounded-xl overflow-hidden shadow-lg"
-              key={key}
-            >
+            <div className="rounded-xl overflow-hidden shadow-lg" key={key}>
               <Image
                 alt={title}
                 height={image.height}
                 src={image.src}
                 width={image.width}
               />
-              <div className="py-4 px-6 flex flex-col gap-3">
+              <div className="px-6 py-4 flex flex-col gap-3">
                 <h3 className="text-xl font-bold">{title}</h3>
                 <p className="flex-grow text-gray-500">{description}</p>
                 <button className="self-start rounded px-4 py-2 bg-gray-800 text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50">
@@ -120,23 +121,45 @@ export default async function Home(): Promise<React.ReactElement> {
             </div>
           ))}
         </div>
-      </div>
-      {/* Cards END */}
+        {/* Cards END */}
 
-      {/* Companies */}
-      {companies.map(company => (
-        <div key={company.id}>
-          {company.name}
-          <Image
-            alt={company.name}
-            height={100}
-            priority
-            src={company.logo}
-            width={100}
-          />
+        {/* Featured companies */}
+        <div className="-mx-1 carousel">
+          {companies.map(company => (
+            <div
+              className="carousel-item w-1/4 min-w-1/4 px-1 flex-col"
+              key={company.id}
+            >
+              <Image
+                alt={company.name}
+                className="w-full"
+                height={900}
+                priority
+                src={assertDefined(company.images[0])}
+                width={1600}
+              />
+              {company.name}
+            </div>
+          ))}
         </div>
-      ))}
-      {/* Companies END */}
+        {/* Featured companies END */}
+
+        {/* Companies */}
+        {companies.map(company => (
+          <div className="flex" key={company.id}>
+            <Image
+              alt={company.name}
+              className="w-aspect-ratio-16/9 h-aspect-ratio-16/9"
+              height={1600}
+              src={assertDefined(company.images[0])}
+              width={900}
+            />
+            {company.name}
+          </div>
+        ))}
+        {/* Companies END */}
+      </div>
+      {/* Contents END */}
     </div>
   );
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,33 @@
+/* eslint-disable no-magic-numbers -- Ok */
+
+import daisyui from "daisyui";
+import flowbite from "flowbite/plugin";
+
 /**
  * @type {import("tailwindcss").Config}
  */
 const config = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  plugins: [],
-  theme: { extend: {} }
+  content: [
+    "./node_modules/flowbite-react/lib/**/*.js",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  plugins: [daisyui, flowbite],
+  theme: {
+    extend: {
+      borderWidth: {
+        1.5: "5px"
+      },
+      height: {
+        "aspect-ratio-16/9": `${0.01 * 900}rem`
+      },
+      minWidth: {
+        "1/3": "33.333333%"
+      },
+      width: {
+        "aspect-ratio-16/9": `${0.01 * 1600}rem`
+      }
+    }
+  }
 };
 
 export default config;
