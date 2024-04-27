bb90a851142b4b4f0dc51d03ebfd930cf0e35648
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-04-28 01:16:53 +0300

    Adjusted contents of category route

--- src/pages/categories/[category].tsx
+++ src/pages/categories/[category].tsx
@@ -5,8 +5,6 @@ import { getCategories, getCompaniesByCategory } from "../../api";
 import Image from "next/image";
 import Layout from "../../Layout";
 import React from "react";
-import { images } from "../../images";
-import { lang } from "../../langs";
 import { useRouter } from "next/router";
 
 const Page: NextPage<Props> = ({ categories, companies }) => {
@@ -16,55 +14,9 @@ const Page: NextPage<Props> = ({ categories, companies }) => {
     return <div>Loading...</div>;
   }
 
-  const cards = [
-    {
-      description: lang.home.card1.description,
-      image: images.digitalSign,
-      title: lang.home.card1.title
-    },
-    {
-      description: lang.home.card2.description,
-      image: images.IPO,
-      title: lang.home.card2.title
-    },
-    {
-      description: lang.home.card3.description,
-      image: images.unicorn,
-      title: lang.home.card3.title
-    }
-  ];
-
   return (
     <Layout categories={categories}>
       <div className="self-center m-9 max-w-screen-lg flex flex-col gap-9">
-        {/* Teaser */}
-        <h2 className="text-center text-xl text-gray-500">
-          {lang.home.teaser}
-        </h2>
-        {/* Teaser END */}
-
-        {/* Cards */}
-        <div className="grid grid-cols-3 gap-3">
-          {cards.map(({ description, image, title }, key) => (
-            <div className="rounded-xl overflow-hidden shadow-lg" key={key}>
-              <Image
-                alt={title}
-                height={image.height}
-                src={image.src}
-                width={image.width}
-              />
-              <div className="px-6 py-4 flex flex-col gap-3">
-                <h3 className="text-xl font-bold">{title}</h3>
-                <p className="flex-grow text-gray-500">{description}</p>
-                <button className="self-start rounded px-4 py-2 bg-gray-800 text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50">
-                  {lang.LearnMore}
-                </button>
-              </div>
-            </div>
-          ))}
-        </div>
-        {/* Cards END */}
-
         {/* Companies */}
         <div className="-mx-1 carousel">
           {companies.docs.map(company => {
