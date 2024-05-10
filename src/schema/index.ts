b7ec08a1bf4a076f320142587570b31529c71bf4
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-05-10 19:57:19 +0300

    Schema version 20240510.1

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,10 +1,8 @@
-import version from "./version.json";
-
-export const { schemaVersion } = version;
-
 export * from "./auth";
 export * from "./categories";
 export * from "./common";
 export * from "./companies";
+export * from "./get-all-options";
 export * from "./routes";
 export * from "./users";
+export { schemaVersion } from "./version.json";
