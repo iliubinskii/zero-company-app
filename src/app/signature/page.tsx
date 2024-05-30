51e0b82e4785a49c79e65414a87576f8c045095d
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-05-30 11:19:02 +0300

    Signature page accessible only to authenticated users

--- src/app/signature/page.tsx
+++ src/app/signature/page.tsx
@@ -1,14 +1,27 @@
+"use client";
+
+import { selectAuthUser, useAppSelector } from "../../services";
 import { DocusealForm } from "@docuseal/react";
+import { Navigate } from "../../components";
 import React from "react";
 import { createPage } from "../../utils";
 
-const Signature = createPage("/signature", () => (
-  <div className="blocks-layout-lg">
-    <DocusealForm
-      email="signer@example.com"
-      src="https://docuseal.co/d/jrJKKG9T8NKhGo"
-    />
-  </div>
-));
+// eslint-disable-next-line no-warning-comments -- Assigned to Daniel
+// TODO
+// Verify authUser.email (only document signatories can sign document)
+const Signature = createPage("/signature", () => {
+  const authUser = useAppSelector(selectAuthUser);
+
+  return authUser ? (
+    <div className="blocks-layout-lg">
+      <DocusealForm
+        email={authUser.email}
+        src="https://docuseal.co/d/jrJKKG9T8NKhGo"
+      />
+    </div>
+  ) : (
+    <Navigate to="/" />
+  );
+});
 
 export default Signature;
