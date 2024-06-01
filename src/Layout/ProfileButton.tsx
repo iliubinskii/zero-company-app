1586928f22cc6c3e2558f2a4bc0472fc0544e380
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-06-02 01:45:04 +0300

    Fix for profile button resizing on initial loading

--- src/Layout/ProfileButton.tsx
+++ src/Layout/ProfileButton.tsx
@@ -14,7 +14,7 @@ const ProfileButton: FC<HTMLAttributes<HTMLDivElement>> = ({
   const authUser = useAppSelector(selectAuthUser);
 
   return (
-    <div className={`w-30 flex justify-end ${className}`.trim()} {...props}>
+    <div className={`w-20 flex justify-end ${className}`.trim()} {...props}>
       <AnimatedLink
         className="px-2 py-3 whitespace-nowrap transition-colors duration-150 hover:text-green-800"
         href={authUser ? "/profile" : `${API_URL}auth/login`}
