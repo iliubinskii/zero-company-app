32958043956293a5f0dec08e08204383340b9d8c
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-06-04 14:39:30 +0300

    Company card additional info on hover

--- src/components/CompanyCard.tsx
+++ src/components/CompanyCard.tsx
@@ -1,25 +1,48 @@
-import type { FC, HTMLAttributes } from "react";
+"use client";
+
 import type { Company } from "../schema";
 import React from "react";
 import { assertDefined } from "../utils";
 
-export const CompanyCard: FC<Props> = ({ company, ...props }) => {
-  const { height, secureUrl, width } = assertDefined(company.images[0]);
+export const CompanyCard: React.FC<Props> = ({ className, company }) => {
+  const image = assertDefined(company.images[0]);
+
+  const [isHovering, setIsHovering] = React.useState(false);
 
   return (
-    <div {...props}>
-      <img
-        alt={company.name}
-        className="w-full"
-        height={height}
-        src={secureUrl}
-        width={width}
-      />
-      {company.name}
+    <div
+      className={`${className} ${isHovering ? "-m-6 relative z-10" : ""}`}
+      onMouseEnter={() => {
+        setIsHovering(true);
+      }}
+      onMouseLeave={() => {
+        setIsHovering(false);
+      }}
+    >
+      <div
+        className={
+          isHovering ? "px-6 pt-6 bg-white rounded-t-md shadow-lg" : ""
+        }
+      >
+        <img
+          alt={company.name}
+          className="w-full"
+          height={image.height}
+          src={image.secureUrl}
+          width={image.width}
+        />
+        {company.name}
+      </div>
+      <div
+        className={`absolute left-0 right-0 rounded-b-md px-6 pb-6 bg-white shadow-lg ${isHovering ? "" : "hidden"}`.trim()}
+      >
+        <div className="text-sm">{company.description}</div>
+      </div>
     </div>
   );
 };
 
-export interface Props extends HTMLAttributes<HTMLDivElement> {
+export interface Props {
+  readonly className?: string | undefined;
   readonly company: Company;
 }
