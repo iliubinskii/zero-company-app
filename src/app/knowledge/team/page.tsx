68a79e9e66170007cd4b819d34126cfef1019855
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-06-17 00:10:13 +0300

    Style fixes

--- src/app/knowledge/team/page.tsx
+++ src/app/knowledge/team/page.tsx
@@ -5,28 +5,26 @@ import React from "react";
 import { members } from "../../../members";
 
 const Page: NextPage = () => (
-  <div className="">
-    <ArticleLayout>
-      <div className="text-center text-4xl font-bold my-8">OUR TEAM</div>
+  <ArticleLayout size="2xl">
+    <div className="mx-auto max-w-screen-md">
+      <div className="text-center text-4xl font-bold my-8">TEAM</div>
       <div className="text-lg leading-relaxed mb-8">
         <p className="text-xl font-semibold italic text-justify">
-          Our team is a group of passionate and dedicated professionals who have
-          come together with a common vision: to create an innovative platform
-          that empowers startup founders to bring their projects to life. With a
+          We are a group of passionate and dedicated professionals who have come
+          together with a common vision: to create an innovative platform that
+          empowers startup founders to bring their projects to life. With a
           diverse range of expertise, from full-stack development and project
           management to quality assurance and user interface design, each member
           contributes their unique skills to ensure the success of our product.
         </p>
       </div>
-    </ArticleLayout>
-    <div className="flex flex-wrap justify-center item-center px-12">
+    </div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 justify-center item-center gap-16 pb-8">
       {members.map(member => (
-        <div className="p-4 md:w-full xl:w-1/2" key={member.id}>
-          <MemberCard member={member} />
-        </div>
+        <MemberCard key={member.id} member={member} />
       ))}
     </div>
-  </div>
+  </ArticleLayout>
 );
 
 export default Page;
--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -6,22 +6,22 @@ export const MemberCard: React.FC<{ member: Member }> = ({ member }) => {
   const avatarSrc = member.avatar ? member.avatar : defaultAvatar;
 
   return (
-    <div className="flex min-w-72 max-w-3xl h-72 items-start p-4 border border-gray-300 rounded-2xl shadow-lg overflow-y-auto">
-      <div className="flex flex-col justify-center items-center mr-4 w-1/3">
+    <div className="flex h-full items-start p-6 border border-gray-300 rounded-2xl shadow-lg overflow-y-auto">
+      <div className="flex flex-col justify-center items-center mr-4 w-1/3 min-w-40">
         <img
           alt={member.name}
           className="w-40 h-40 rounded-full p-2"
           src={avatarSrc}
         />
         {member.roles.map((role, index) => (
-          <span className="font-sb" key={index}>
+          <div className="font-sb" key={index}>
             {role}
-          </span>
+          </div>
         ))}
       </div>
       <div className="flex flex-col w-2/3">
-        <span className="text-lg font-bold pb-2">{member.name}</span>
-        <span className="overflow-y-auto max-44">{member.description}</span>
+        <div className="text-lg font-bold pb-2">{member.name}</div>
+        <div className="overflow-y-auto max-44 pb-4">{member.description}</div>
       </div>
     </div>
   );
