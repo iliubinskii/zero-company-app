3e16460a85dbcf3e3f42d0ba22259f9de9d77340
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-08-06 18:15:39 +0300

    fix: For failed Vercel deployment

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,12 +1,16 @@
 import { ClientPage } from "./ClientPage";
 import type { NextPage } from "next";
-import React from "react";
+import React, { Suspense } from "react";
 import { api } from "../../api";
 
 const Page: NextPage = async () => {
   const categories = await api.getCategoriesSrv();
 
-  return <ClientPage categories={categories.docs} />;
+  return (
+    <Suspense>
+      <ClientPage categories={categories.docs} />
+    </Suspense>
+  );
 };
 
 export default Page;
