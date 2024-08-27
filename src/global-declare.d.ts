3b0809da009b61533ae3d6babab6c5d191cf1858
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-08-27 21:49:29 +0300

    refactor: Edited global.d.ts

--- src/global-declare.d.ts
+++ src/global-declare.d.ts
@@ -1,20 +1,13 @@
 /* eslint import/no-extraneous-dependencies: ["warn", { "devDependencies": true }] -- Ok */
 
-// eslint-disable-next-line misc/comment-spacing -- Ok
-/// <reference types="jest-extended" />
-
 import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
 import type { expect } from "@jest/globals";
+import type jestExtended from "jest-extended";
+
+type JestExtended = typeof jestExtended;
 
 declare module "@jest/expect" {
   export interface Matchers<R>
-    // eslint-disable-next-line no-undef -- Ok
-    extends CustomMatchers<R>,
+    extends JestExtended,
       TestingLibraryMatchers<typeof expect.stringContaining, R> {}
-}
-
-declare module "next/jest.js" {
-  export default function nextJest(
-    options: unknown
-  ): (config: unknown) => unknown;
 }
